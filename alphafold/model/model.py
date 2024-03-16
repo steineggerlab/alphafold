e4b9dd1d1c135ac6af76c63336430d929d12e801
Poko18 <tadej.satler@gmail.com>
2024-03-16 22:13:58 +0100

    Refactor model.py: Add initial guess for all_atom_positions

--- alphafold/model/model.py
+++ alphafold/model/model.py
@@ -147,12 +147,20 @@ class RunModel:
       num_ensemble = self.config.data.eval.num_ensemble
       L = aatype.shape[1]
     
-    # initialize
+
+  
 
     zeros = lambda shape: np.zeros(shape, dtype=np.float16)
     prev = {'prev_msa_first_row': zeros([L,256]),
-            'prev_pair':          zeros([L,L,128]),
-            'prev_pos':           zeros([L,37,3])}
+            'prev_pair':          zeros([L,L,128])}
+    
+    # initial guess
+    if "all_atom_positions" in feat:
+      print("INFO: all_atom_positions provided, using as initial guess")
+      prev["prev_pos"] = feat["all_atom_positions"]
+    else:
+      print("WARNING: all_atom_positions not provided, using zeros as initial guess")
+      prev["prev_pos"] = np.zeros([L,37,3])
     
     def run(key, feat, prev):
       def _jnp_to_np(x):
