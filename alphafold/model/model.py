225359c391fcd57be1b5ac59a4c81ccd5729d78d
Milot Mirdita <milot@mirdita.de>
2026-09-14 11:35:14 +0900

    Match the initial guess dtype to prev_pos so the trunk compiles once

--- alphafold/model/model.py
+++ alphafold/model/model.py
@@ -158,7 +158,7 @@ class RunModel:
     # initial guess
     if "all_atom_positions" in feat:
       logging.info("INFO: using provided all_atom_positions as initial guess")
-      prev["prev_pos"] = feat["all_atom_positions"]
+      prev["prev_pos"] = np.asarray(feat["all_atom_positions"], np.float16)
     else:
       prev["prev_pos"] = zeros([L,37,3])
     
